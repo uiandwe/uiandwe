54a5d52da492d073fd82a6dd8f5013ab65004f64
hyeon seung jae <uiandwe@gmail.com>
2021-07-10 13:56:58 +0900

    Update build-svg.js

--- build-svg.js
+++ build-svg.js
@@ -6,6 +6,13 @@ fs.readFile("template.svg", "utf-8", (error, data) => {
   if (error) {
     return;
   }
+  
+  data = data.replace('{name}', "")
+//   data = data.replace('{degC}', degC)
+//   data = data.replace('{weatherEmoji}', emojis[icon])
+//   data = data.replace('{psTime}', psTime)
+//   data = data.replace('{todayDay}', todayDay)
+//   data = data.replace('{dayBubbleWidth}', dayBubbleWidths[todayDay])
 
   data = fs.writeFile("chat.svg", data, (err) => {
     if (err) {
